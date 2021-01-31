e32aa6244489cd6431fc375630031ab0a83929eb
Tony Air <tony@thewebdevelopmentagency.com>
2021-01-31 19:08:55 +0700

    IMPR: minor fixtures

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MetaWindow from './_window.jsx';
 
-const M = ReactDOM.render(<MetaWindow />, document.getElementById('App'));
-ReactDOM.render(<MetaWindow />, document.getElementById('App'));
+const M = ReactDOM.render(
+	<MetaWindow />,
+	document.getElementById('MetaLightboxApp'),
+);
 
 // display custom HTML content manually using JS
 //M.setContent('<b>ZZZZZZZZZAAAA11<a href="/">BBBB</a>122</b>');
