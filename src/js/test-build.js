7ae9d3fd846f2bc2777a4ffb61e91f70c248bdb8
Tony Air <tony@twma.pro>
2021-08-11 19:06:49 +0200

    IMPR: remove images at the module

--- src/js/test-build.js
+++ src/js/test-build.js
@@ -8,12 +8,12 @@ import ui from './app';
 import '../scss/app.scss';
 import '../scss/test-build.scss';
 
-function importAll(r) {
+/*function importAll(r) {
   return r.keys().map(r);
 }
 
 const images = importAll(
   require.context('../img/', false, /\.(png|jpe?g|svg)$/),
-);
+);*/
 
 export default ui;
